3964a46dd0fafe81ce25e07a7709e72f4a821b6f
Eunjung Nam <eunjungnam@eunjungs-air.lan>
2021-02-03 11:33:26 -0500

    add compose input

--- app.js
+++ app.js
@@ -27,6 +27,9 @@ app.get("/about", function (req, res) {
 app.get("/contact", function (req, res) {
   res.render("contact", { contactConent: contactContent });
 });
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
