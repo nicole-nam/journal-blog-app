96633ea815505e3f2ec7e793fd011c7adbcd6260
Eunjung Nam <eunjungnam@eunjungs-air.lan>
2021-02-03 14:32:43 -0500

    add rout posts and params

--- app.js
+++ app.js
@@ -34,6 +34,9 @@ app.get("/contact", function (req, res) {
 app.get("/compose", function (req, res) {
   res.render("compose");
 });
+app.get("/posts/:postName", function (req, res) {
+  console.log(req.params.postName);
+});
 
 app.post("/compose", function (req, res) {
   const post = {
