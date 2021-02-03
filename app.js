a95b74c3e21c0d0a94c4793bdcaf10afab41106d
Eunjung Nam <eunjungnam@eunjungs-air.lan>
2021-02-03 12:14:52 -0500

    add empty array called posts and push post object into posts array

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+let posts = [];
 
 app.set("view engine", "ejs");
 
@@ -32,14 +33,11 @@ app.get("/compose", function (req, res) {
 });
 
 app.post("/compose", function (req, res) {
-  console.log(req.body.postTitle);
-  console.log(req.body.postBody);
   const post = {
     title: req.body.postTitle,
     content: req.body.postBody,
   };
-
-  console.log(post);
+  posts.push(post);
 });
 
 app.listen(3000, function () {
