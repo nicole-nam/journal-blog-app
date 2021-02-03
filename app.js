3ade0f47f6f10b19eef54c50e91a821361f64d29
Eunjung Nam <eunjungnam@eunjungs-air.lan>
2021-02-03 12:11:09 -0500

    create an object called post to store form input in app.js

--- app.js
+++ app.js
@@ -34,6 +34,12 @@ app.get("/compose", function (req, res) {
 app.post("/compose", function (req, res) {
   console.log(req.body.postTitle);
   console.log(req.body.postBody);
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  };
+
+  console.log(post);
 });
 
 app.listen(3000, function () {
